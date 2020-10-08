26a62cc9a5643638f369e13206d80b9d4a8fce8f
alincorpodean <72561107+alincorpodean@users.noreply.github.com>
2020-10-08 19:11:55 +0300

    Add files via upload

--- camera.js
+++ camera.js
@@ -1,26 +1,33 @@
-var video=document.getElementById("video");
-//------------
+var video = document.getElementById("video");
+
 function on_cam_success(stream)
 {
-    video.srcObject=stream;
+	video.srcObject = stream;
 }
-//-----------
+
 function on_cam_error(err)
+
 {
-    alert("error."+err.message);
+	alert("error." + err.message);
 }
-//-------------
-var constraints={audio:false,video:true};
+
+var constraints = {audio: false, video: true};
+
 navigator.mediaDevices.getUserMedia(constraints)
+
 .then(on_cam_success)
+
 .catch(on_cam_error);
-//--------------
+
 function capteaza()
 {
-    var c=document.getElementById("canvas");
-    c.width=video.width;
-    c.height=video.height;
-    var ctx=c.getContext("2d");
-    ctx.drawImage(video,0,0,640,480);
-   
+	var c = document.getElementById("canvas");
+	
+	c.width = video.width;
+	
+	c.height = video.height;
+	
+	var ctx = c.getContext("2d");
+	
+	ctx.drawImage(video, 0,0,640,480);
 }
